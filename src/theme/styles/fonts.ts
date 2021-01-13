ab204b1f259a98d3fa0c2bacd4171e043cc9292c
David Sato <david.sato64@gmail.com>
2021-01-13 18:31:09 +0000

    Fonts - added type/theme to file&theme
    1. own fonts folder with fontFaces + styles
    2. getter function

--- src/theme/styles/fonts.ts
+++ src/theme/styles/fonts.ts
@@ -4,7 +4,9 @@ import InterRegular from 'assets/fonts/Inter-Regular.woff2'
 import InterBold from 'assets/fonts/Inter-Bold.woff2'
 import RobotoMono from 'assets/fonts/RobotoMono-Regular.woff2'
 
-const fontFace = css`
+import { Theme } from 'theme'
+
+export const fontFace = css`
   @font-face {
     font-family: 'Inter';
     src: url(${InterRegular}) format('woff2');
@@ -24,5 +26,45 @@ const fontFace = css`
     font-style: normal;
   }
 `
+export interface Fonts {
+  fontDefault: string
+  fontArial: string
+  fontSizeNormal: string
+  fontSizeSmall: string
+  fontSizeLarge: string
+  fontSizeXLarge: string
+  fontSizeXXLarge: string
+  fontWeightNormal: string
+  fontWeightMedium: string
+  fontWeightBold: string
+}
+
+const DEFAULT_FONT = {
+  fontDefault: '"Inter", "Helvetica Neue"',
+  fontArial: 'Arial',
+  fontSizeNormal: '1.2rem',
+  fontSizeSmall: '1.1rem',
+  fontSizeLarge: '1.3',
+  fontSizeXLarge: '1.4rem',
+  fontSizeXXLarge: '1.5rem',
+  fontWeightNormal: '400',
+  fontWeightMedium: '500',
+  fontWeightBold: '700',
+}
+
+export function getFonts(mode: Theme): Fonts {
+  let FONT: Fonts
+
+  switch (mode) {
+    case Theme.LIGHT:
+    case Theme.DARK:
+      FONT = DEFAULT_FONT
+      break
+    default:
+      FONT = DEFAULT_FONT
+      break
+  }
+  return FONT
+}
 
 export default fontFace
