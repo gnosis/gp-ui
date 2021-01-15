2b4902dd3df5f2a59681951fe16c260be3c0631b
David Sato <david.sato64@gmail.com>
2021-01-15 16:11:43 +0000

    add to theme and export from styles

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -10,7 +10,24 @@ import {
 } from 'styled-components'
 
 import { useThemeMode } from 'hooks/useThemeManager'
-import { getFonts, getThemePalette } from './styles'
+import { getFonts, getThemePalette, mediaWidthTemplates as mediaQueries } from './styles'
+import { Theme } from './types'
+
+const getBaseTheme = ({
+  mode,
+  componentKey,
+}: {
+  mode: Theme
+  componentKey?: Partial<DefaultTheme['componentKey']>
+}): Pick<DefaultTheme, 'mode' | 'componentKey' | 'mediaQueries'> => ({
+  mode,
+  // unfold in any extensions
+  // for example to make big/small buttons -> see src/components/Button ThemeWrappedButtonBase
+  // to see it in action
+  componentKey,
+  // media queries
+  mediaQueries,
+})
 
 // This type is all React.ReactElement & StyledComponents combined
 type ReactOrStyledNode = React.ReactElement &
@@ -21,25 +38,21 @@ const ThemeProvider: React.FC<{ componentKey?: Partial<DefaultTheme['componentKe
   children,
   componentKey,
 }) => {
-  const themeMode = useThemeMode()
+  const mode = useThemeMode()
 
   const themeObject = useMemo(() => {
-    const themePalette = getThemePalette(themeMode)
-    const fontPalette = getFonts(themeMode)
+    const themePalette = getThemePalette(mode)
+    const fontPalette = getFonts(mode)
 
     const computedTheme: DefaultTheme = {
       // Compute the app colour pallette using the passed in themeMode
       ...themePalette,
       ...fontPalette,
-      mode: themeMode,
-      // unfold in any extensions
-      // for example to make big/small buttons -> see src/components/Button ThemeWrappedButtonBase
-      // to see it in action
-      componentKey,
+      ...getBaseTheme({ mode, componentKey }),
     }
 
     return computedTheme
-  }, [themeMode, componentKey])
+  }, [mode, componentKey])
 
   // We want to pass the ThemeProvider theme to all children implicitly, no need to manually pass it
   return (
