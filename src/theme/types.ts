878c926aa092de8e2cb3be5c66c20a8e454dd1f3
David Sato <david.sato64@gmail.com>
2021-01-16 12:11:19 +0000

    add mediaQueries alias mq to types
    1. add descriptive comments

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -1,4 +1,4 @@
-import { Colors, Fonts } from './styles'
+import { Colors, Fonts, MediaWidth } from './styles'
 
 export enum Theme {
   DARK = 'DARK',
@@ -8,24 +8,42 @@ export enum Theme {
 export const THEME_LIST = Object.entries(Theme)
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends Colors, Fonts {
+  export interface DefaultTheme extends DefaultThemeAliases, Colors, Fonts {
     // theming
     mode: Theme
     // used to key in on component variants
     componentKey?: keyof JSX.IntrinsicElements
-    // Media query templates
-    // can be used like:
-    /* 
-    theme.mediaQueries.upToSmall`
-      font-size: 2rem;
-      margin: 0;
-    `
-    */
-    mediaQueries: {
-      upToExtraSmall: ThemedCssFunction<DefaultTheme>
-      upToSmall: ThemedCssFunction<DefaultTheme>
-      upToMedium: ThemedCssFunction<DefaultTheme>
-      upToLarge: ThemedCssFunction<DefaultTheme>
-    }
+    /**
+     * @name mediaQueries
+     *
+     * @example theme.mediaQueries.upToMedium` font-size: larger; color: red; `
+     *
+     * @example {
+     *  upToExtraSmall: 500,
+     *  upToSmall: 720,
+     *  upToMedium: 960,
+     *  upToLarge: 1280,
+     *  tabletPortrait: 720 to 960, orientation: portrait,
+     *  tabletLandscape: 720 to 960, orientation: landscape,
+     * }
+     */
+    mediaQueries: MediaWidth
+  }
+  interface DefaultThemeAliases {
+    /**
+     * @name mq - alias for mediaQueries
+     *
+     * @example theme.mq.upToMedium` font-size: larger; color: red; `
+     *
+     * @example {
+     *  upToExtraSmall: 500,
+     *  upToSmall: 720,
+     *  upToMedium: 960,
+     *  upToLarge: 1280,
+     *  tabletPortrait: 720 to 960, orientation: portrait,
+     *  tabletLandscape: 720 to 960, orientation: landscape,
+     * }
+     */
+    mq: DefaultTheme['mediaQueries']
   }
 }
