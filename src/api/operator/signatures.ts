29db54a2d520eaaa23d53cbb519aef4f641ead7d
Anxo Rodriguez <anxolin@gmail.com>
2021-01-22 16:27:52 +0100

    Remove comments

--- src/api/operator/signatures.ts
+++ src/api/operator/signatures.ts
@@ -13,35 +13,10 @@ export interface SignOrderParams {
   order: UnsignedOrder
 }
 
-// posted to /api/v1/orders on Order creation
-// serializable, so no BigNumbers
-//  See https://protocol-rinkeby.dev.gnosisdev.com/api/
 export interface OrderCreation extends UnsignedOrder {
-  // TODO: I commented this because I expect the API and contract to follow the same structure for the order data. confirm and delete this comment
-  signature: string // 65 bytes encoded as hex without `0x` prefix. v + r + s from the spec
+  signature: string // 65 bytes encoded as hex without `0x` prefix. r + s + v from the spec
 }
 
-// TODO: We cannot make use of the NPM exported enum, see https://babeljs.io/docs/en/babel-plugin-transform-typescript#caveats
-//      use workaround?
-// /**
-//  * The signing scheme used to sign the order.
-//  */
-// export declare const enum SigningScheme {
-//   /**
-//    * The EIP-712 typed data signing scheme. This is the preferred scheme as it
-//    * provides more infomation to wallets performing the signature on the data
-//    * being signed.
-//    *
-//    * <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#definition-of-domainseparator>
-//    */
-//   TYPED_DATA = 0,
-//   /**
-//    * The generic message signing scheme.
-//    */
-//   MESSAGE = 1
-// }
-
-// TODO: For now, instead of using enums (see todo above)
 const TYPED_DATA_SIGNING_SCHEME = 0
 
 function _getDomain(networkId: Network): TypedDataDomain {
