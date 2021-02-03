7b1a3144a39832161aea88e4eaeccf89b24cadcf
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-03 14:52:00 -0800

    Added unittest for getOrderExecutedPrice

--- test/api/OperatorApi/orderPrice.test.ts
+++ test/api/OperatorApi/orderPrice.test.ts
@@ -3,37 +3,63 @@ import BigNumber from 'bignumber.js'
 import { ONE_BIG_NUMBER, ONE_HUNDRED_BIG_NUMBER, TEN_BIG_NUMBER } from 'const'
 
 import { RawOrder } from 'api/operator'
-import { getOrderLimitPrice } from 'api/operator/utils'
+import { getOrderExecutedPrice, getOrderLimitPrice, GetOrderPriceParams } from 'api/operator/utils'
 
 import { ORDER } from '../../data'
 
 const ZERO_DOT_ONE = new BigNumber('0.1')
 
-function _assertOrderPrice(order: RawOrder): void {
+function _assertOrderPrice(order: RawOrder, getPriceFn: (params: GetOrderPriceParams) => BigNumber): void {
   test('Buy token decimals == sell token decimals', () => {
-    expect(getOrderLimitPrice({ order, buyTokenDecimals: 2, sellTokenDecimals: 2 })).toEqual(TEN_BIG_NUMBER)
+    expect(getPriceFn({ order, buyTokenDecimals: 2, sellTokenDecimals: 2 })).toEqual(TEN_BIG_NUMBER)
   })
   test('Buy token decimals > sell token decimals', () => {
-    expect(getOrderLimitPrice({ order, buyTokenDecimals: 2, sellTokenDecimals: 1 })).toEqual(ONE_BIG_NUMBER)
+    expect(getPriceFn({ order, buyTokenDecimals: 2, sellTokenDecimals: 1 })).toEqual(ONE_BIG_NUMBER)
   })
   test('Buy token decimals < sell token decimals', () => {
-    expect(getOrderLimitPrice({ order, buyTokenDecimals: 1, sellTokenDecimals: 2 })).toEqual(ONE_HUNDRED_BIG_NUMBER)
+    expect(getPriceFn({ order, buyTokenDecimals: 1, sellTokenDecimals: 2 })).toEqual(ONE_HUNDRED_BIG_NUMBER)
   })
   test('Inverted price', () => {
-    expect(getOrderLimitPrice({ order, buyTokenDecimals: 2, sellTokenDecimals: 2, inverted: true })).toEqual(
-      ZERO_DOT_ONE,
-    )
+    expect(getPriceFn({ order, buyTokenDecimals: 2, sellTokenDecimals: 2, inverted: true })).toEqual(ZERO_DOT_ONE)
   })
 }
 
-describe('Buy order', () => {
-  const order: RawOrder = { ...ORDER, kind: 'buy', buyAmount: '1000', sellAmount: '100' }
+describe('Limit price', () => {
+  describe('Buy order', () => {
+    const order: RawOrder = { ...ORDER, kind: 'buy', buyAmount: '1000', sellAmount: '100' }
 
-  _assertOrderPrice(order)
+    _assertOrderPrice(order, getOrderLimitPrice)
+  })
+
+  describe('Sell order', () => {
+    const order: RawOrder = { ...ORDER, kind: 'sell', buyAmount: '1000', sellAmount: '100' }
+
+    _assertOrderPrice(order, getOrderLimitPrice)
+  })
 })
 
-describe('Sell order', () => {
-  const order: RawOrder = { ...ORDER, kind: 'sell', buyAmount: '1000', sellAmount: '100' }
+describe('Executed price', () => {
+  describe('Buy order', () => {
+    const order: RawOrder = {
+      ...ORDER,
+      kind: 'buy',
+      executedBuyAmount: '1000',
+      executedSellAmount: '110',
+      executedFeeAmount: '10',
+    }
 
-  _assertOrderPrice(order)
+    _assertOrderPrice(order, getOrderExecutedPrice)
+  })
+
+  describe('Sell order', () => {
+    const order: RawOrder = {
+      ...ORDER,
+      kind: 'sell',
+      executedBuyAmount: '1000',
+      executedSellAmount: '110',
+      executedFeeAmount: '10',
+    }
+
+    _assertOrderPrice(order, getOrderExecutedPrice)
+  })
 })
