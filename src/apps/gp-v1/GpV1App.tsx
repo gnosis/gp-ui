91bbf5d8da6bd5a05993bbd9a1c723c4493d38be
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-04 13:29:45 -0800

    Renamed SwapAppV1 to GpV1App

--- src/apps/gp-v1/GpV1App.tsx
+++ src/apps/gp-v1/GpV1App.tsx
@@ -113,7 +113,7 @@ function getInitialUrl(): string {
 const Router: typeof BrowserRouter & typeof HashRouter = (window as any).IS_IPFS ? HashRouter : BrowserRouter
 const initialUrl = getInitialUrl()
 
-export const SwapAppV1: React.FC = () => {
+export const GpV1App: React.FC = () => {
   // Deal with incorrect network
   useNetworkCheck()
 
@@ -149,7 +149,7 @@ export const SwapAppV1: React.FC = () => {
 
 export default hot(
   withGlobalContext(
-    SwapAppV1,
+    GpV1App,
     // Initial State
     INITIAL_STATE,
     rootReducer,
