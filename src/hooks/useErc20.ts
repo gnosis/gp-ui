156894d328e3b141032e30f2545ee136436ad896
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-09 16:26:48 -0800

    Using erc20s state hooks on useErc20 hook

--- src/hooks/useErc20.ts
+++ src/hooks/useErc20.ts
@@ -1,38 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import { TokenErc20 } from '@gnosis.pm/dex-js'
 
 import { Network } from 'types'
 
-import { buildErc20Key, saveSingleErc20 } from 'state/erc20'
-
-import useGlobalState from 'hooks/useGlobalState'
+import {
+  useGetErc20FromGlobalState,
+  useSaveErc20sToGlobalState,
+  SingleErc20State,
+} from 'state/erc20'
 
 import { getErc20Info } from 'services/helpers'
 
 import { web3, erc20Api } from 'apps/explorer/api'
 
-import { ExplorerAppState } from 'apps/explorer/state'
+type UseErc20Params = { address?: string; networkId?: Network }
 
-type Params = {
-  address?: string
-  networkId?: Network
-}
+type Return<V> = { isLoading: boolean; error?: string; value: V }
 
-type Return = { isLoading: boolean; error?: string; value?: TokenErc20 }
-
-export function useErc20(params: Params): Return {
+/**
+ * Fetches single erc20 token details for given network and address
+ *
+ * Tries to get it from globalState.
+ * If not found, tries to get it from the network.
+ * Saves to globalState if found.
+ * Value is `null` when not found.
+ * Returns `isLoading` to indicate whether fetching the value
+ * Returns `error` with the error message, if any.
+ */
+export function useErc20(params: UseErc20Params): Return<SingleErc20State> {
   const { address, networkId } = params
 
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('')
 
-  const [{ erc20s }, dispatch] = useGlobalState<ExplorerAppState>()
+  const getErc20 = useGetErc20FromGlobalState(networkId)
+  const saveErc20s = useSaveErc20sToGlobalState(networkId)
 
-  // Initial state from global state
-  const [erc20, setErc20] = useState<TokenErc20 | undefined>(
-    address && networkId ? erc20s.get(buildErc20Key(networkId, address)) : undefined,
-  )
+  // Local copy of globalState
+  const erc20 = useMemo(() => getErc20(address), [address, getErc20])
 
   useEffect(() => {
     // Only try to fetch it if not on global state
@@ -44,8 +50,7 @@ export function useErc20(params: Params): Return {
         .then((fetchedErc20) => {
           // When not found, it returns null
           if (fetchedErc20) {
-            dispatch(saveSingleErc20(fetchedErc20, networkId))
-            setErc20(fetchedErc20)
+            saveErc20s([fetchedErc20])
           }
         })
         .catch((e) => {
@@ -59,7 +64,7 @@ export function useErc20(params: Params): Return {
           setIsLoading(false)
         })
     }
-  }, [address, dispatch, erc20, networkId])
+  }, [address, erc20, networkId, saveErc20s])
 
   return { isLoading, error, value: erc20 }
 }
