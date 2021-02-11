a1073d7f2f4e085facd1f113883b8149798efe91
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-11 13:11:36 -0800

    createWeb3Api now caches instances and accepts optional provider param

--- src/api/web3/index.ts
+++ src/api/web3/index.ts
@@ -5,9 +5,16 @@ import { ETH_NODE_URL } from 'const'
 // TODO connect to mainnet if we need AUTOCONNECT at all
 export const getDefaultProvider = (): string | null => (process.env.NODE_ENV === 'test' ? null : ETH_NODE_URL)
 
-export function createWeb3Api(): Web3 {
+const web3cache = {}
+
+export function createWeb3Api(provider?: string): Web3 {
+  const _provider = provider || getDefaultProvider() || ''
+
+  if (web3cache[_provider]) {
+    return web3cache[_provider]
+  }
   // TODO: Create an `EthereumApi` https://github.com/gnosis/gp-v1-ui/issues/331
-  const web3 = new Web3(getDefaultProvider())
+  const web3 = new Web3(_provider)
   // `handleRevert = true` makes `require` failures to throw
   // For more details see https://github.com/gnosis/gp-v1-ui/issues/511
   web3.eth['handleRevert'] = true
@@ -16,5 +23,7 @@ export function createWeb3Api(): Web3 {
     // Only function that needs to be mocked so far. We can add more and add extra logic as needed
     web3.eth.getCode = async (address: string): Promise<string> => address
   }
+
+  web3cache[_provider] = web3
   return web3
 }
