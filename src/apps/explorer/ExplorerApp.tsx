960a93a9375e7ec6321519beabce898473464be5
Anxo Rodriguez <anxolin@gmail.com>
2021-02-11 16:28:41 +0100

    Revert "Use a hook instead of a component for the updater"
    This reverts commit 2bd8870a804d8f481078714c4172c4438541ae11.

--- src/apps/explorer/ExplorerApp.tsx
+++ src/apps/explorer/ExplorerApp.tsx
@@ -11,7 +11,7 @@ import { rootReducer, INITIAL_STATE } from 'apps/explorer/state'
 import { GenericLayout } from 'components/layout'
 import { Navigation } from 'components/layout/GenericLayout/Navigation'
 import { Header } from 'components/layout/GenericLayout/Header'
-import { useNetworkUpdater } from 'state/network'
+import { NetworkUpdater } from 'state/network'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Router: typeof BrowserRouter & typeof HashRouter = (window as any).IS_IPFS ? HashRouter : BrowserRouter
@@ -56,14 +56,16 @@ const HEADER = (
   </Header>
 )
 
+export const Updaters: React.FC = () => <NetworkUpdater />
+
 export const ExplorerApp: React.FC = () => {
-  // Updaters
-  useNetworkUpdater()
+  // Deal with incorrect network
   useNetworkCheck()
 
   return (
     <>
       <Router basename={process.env.BASE_URL}>
+        <Updaters />
         <GenericLayout header={HEADER}>
           <React.Suspense fallback={null}>
             <Switch>
--- src/state/network/hooks.ts
+++ src/state/network/hooks.ts
@@ -1,11 +1,6 @@
-import { useEffect } from 'react'
-import { useLocation } from 'react-router'
-
-import { Network } from 'types'
-import useGlobalState from 'hooks/useGlobalState'
-
-import { setNetwork } from './actions'
 import { ExplorerAppState } from 'apps/explorer/state'
+import useGlobalState from 'hooks/useGlobalState'
+import { Network } from 'types'
 
 export function useNetworkId(): Network | null {
   const [{ networkId }] = useGlobalState<ExplorerAppState>()
@@ -17,34 +12,4 @@ export function useNetworkOrDefault(): Network {
   const [{ networkId }] = useGlobalState<ExplorerAppState>()
 
   return networkId || Network.Mainnet
-}
-
-function getNetworkId(network: string | undefined): Network {
-  switch (network) {
-    case 'rinkeby':
-      return Network.Rinkeby
-    case 'xdai':
-      return Network.xDAI
-    default:
-      return Network.Mainnet
-  }
-}
-
-export function useNetworkUpdater(): void {
-  // TODO: why not using useDispatch from https://react-redux.js.org/introduction/quick-start
-  // const dispatch = useDispatch()
-  const [, dispatch] = useGlobalState()
-  const currentNetworkId = useNetworkId()
-  const location = useLocation()
-
-  useEffect(() => {
-    const networkMatchArray = location.pathname.match('^/(rinkeby|xdai)')
-    const network = networkMatchArray && networkMatchArray.length > 0 ? networkMatchArray[1] : undefined
-    const networkId = getNetworkId(network)
-
-    // Update the network if it's different
-    if (currentNetworkId !== networkId) {
-      dispatch(setNetwork(networkId))
-    }
-  }, [location, currentNetworkId, dispatch])
 }
