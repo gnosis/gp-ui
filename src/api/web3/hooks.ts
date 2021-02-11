5c9701a037f92f7d6f677794131637b3092fe03b
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-11 13:34:27 -0800

    Added second approach for dealing with web3 provider network aware

--- src/api/web3/hooks.ts
+++ src/api/web3/hooks.ts
@@ -1,39 +1,16 @@
 import { useMemo } from 'react'
 import Web3 from 'web3'
 
-import { getNetworkFromId } from '@gnosis.pm/dex-js'
-
 import { useNetworkId } from 'state/network'
 
-import { Network } from 'types'
+import { createWeb3Api, getProviderByNetwork } from '.'
 
-import { INFURA_ID } from 'const'
-
-import { createWeb3Api } from '.'
-
-function infuraProvider(networkId: Network): string {
-  // INFURA_ID relies on mesa `config` file logic.
-  // We can be independent of that config by relying on the env var directly
-  if (!INFURA_ID) {
-    throw new Error(`INFURA_ID not set`)
-  }
-  return `wss://${getNetworkFromId(networkId).toLowerCase()}.infura.io/ws/v3/${INFURA_ID}`
-}
-
-function infuraProviderByNetworkId(networkId: Network | null): string | undefined {
-  switch (networkId) {
-    case Network.Mainnet:
-    case Network.Rinkeby:
-    case Network.xDAI:
-      return infuraProvider(networkId)
-    default:
-      return undefined
-  }
-}
-
+// Approach 1: create one web3 instance per network
+// Must be used inside components, since it's a hook
+// Can also be used outside by calling `createWeb3Api` directly
 export function useWeb3(): Web3 {
   const networkId = useNetworkId()
-  const provider = infuraProviderByNetworkId(networkId)
+  const provider = getProviderByNetwork(networkId)
 
   // createWeb3 caches instances per provider, safe to call multiple times
   return useMemo(() => createWeb3Api(provider), [provider])
--- src/api/web3/index.ts
+++ src/api/web3/index.ts
@@ -1,6 +1,9 @@
 import Web3 from 'web3'
+import { getNetworkFromId } from '@gnosis.pm/dex-js'
 
-import { ETH_NODE_URL } from 'const'
+import { Network } from 'types'
+
+import { ETH_NODE_URL, INFURA_ID } from 'const'
 
 // TODO connect to mainnet if we need AUTOCONNECT at all
 export const getDefaultProvider = (): string | null => (process.env.NODE_ENV === 'test' ? null : ETH_NODE_URL)
@@ -26,4 +29,39 @@ export function createWeb3Api(provider?: string): Web3 {
 
   web3cache[_provider] = web3
   return web3
+}
+
+function infuraProvider(networkId: Network): string {
+  // INFURA_ID relies on mesa `config` file logic.
+  // We can be independent of that config by relying on the env var directly
+  if (!INFURA_ID) {
+    throw new Error(`INFURA_ID not set`)
+  }
+  return `wss://${getNetworkFromId(networkId).toLowerCase()}.infura.io/ws/v3/${INFURA_ID}`
+}
+
+// For now only infura provider is available
+export function getProviderByNetwork(networkId: Network | null): string | undefined {
+  switch (networkId) {
+    case Network.Mainnet:
+    case Network.Rinkeby:
+    case Network.xDAI:
+      return infuraProvider(networkId)
+    default:
+      return undefined
+  }
+}
+
+// Approach 2: update the provider in a single web3 instance
+// Advantage is that regular APIs that require web3 instance should work without any changes
+// Also, there's no change to consumers currently importing from <app>/api module
+// Side effect is applied at reducer level (state/network/updater module)
+export function updateWeb3Provider(web3: Web3, networkId?: Network | null): void {
+  if (!networkId) {
+    return
+  }
+
+  const provider = getProviderByNetwork(networkId)
+
+  provider && web3.setProvider(provider)
 }
