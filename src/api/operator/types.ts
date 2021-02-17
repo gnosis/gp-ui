5be82d8f723e9d988b6a1922928bd37202848a8d
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-16 18:02:53 -0800

    Added "rich" order type Order

--- src/api/operator/types.ts
+++ src/api/operator/types.ts
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js'
+
 import { Network } from 'types'
 
 export type OrderID = string
@@ -36,6 +38,30 @@ export type RawOrder = {
   kind: OrderKind
   partiallyFillable: boolean
   signature: string
+}
+
+/**
+ * Enriched Order type.
+ * Applies some transformations on the raw api data.
+ * Some fields are kept as is.
+ */
+export type Order = Pick<RawOrder, 'owner' | 'uid' | 'appData' | 'kind' | 'partiallyFillable' | 'signature'> & {
+  creationDate: Date
+  expirationDate: Date
+  buyTokenAddress: string
+  sellTokenAddress: string
+  buyAmount: BigNumber
+  sellAmount: BigNumber
+  executedBuyAmount: BigNumber
+  executedSellAmount: BigNumber
+  feeAmount: BigNumber
+  executedFeeAmount: BigNumber
+  cancelled: boolean
+  // status: OrderStatus // better not have the status because it depends on `expirationDate`
+  filledAmount: BigNumber
+  filledPercentage: BigNumber
+  // limitPrice: BigNumber  // cant have prices because they depend on token decimals
+  // executedPrice: BigNumber
 }
 
 type WithNetworkId = { networkId: Network }
