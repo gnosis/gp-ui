e0cbffda09194f9e3a6dcdd471a2b32ff0d73718
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-17 12:21:59 -0800

    useOperatorOrder hook now optionally updates order every x sec

--- src/hooks/useOperatorOrder.ts
+++ src/hooks/useOperatorOrder.ts
@@ -8,10 +8,20 @@ import { getOrder } from 'api/operator/operatorApi'
 import { useNetworkId } from 'state/network'
 import { useMultipleErc20 } from './useErc20'
 
-export function useOrder(orderId: string): { order: Order | null; error?: string; isLoading: boolean } {
+type UseOrderResult = {
+  order: Order | null
+  error?: string
+  isLoading: boolean
+  forceUpdate: () => void
+}
+
+export function useOrder(orderId: string): UseOrderResult {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('')
   const [order, setOrder] = useState<Order | null>(null)
+  // Hack to force component to update itself on demand
+  const [forcedUpdate, setForcedUpdate] = useState({})
+  const forceUpdate = (): void => setForcedUpdate({})
 
   const networkId = useNetworkId()
 
@@ -37,12 +47,12 @@ export function useOrder(orderId: string): { order: Order | null; error?: string
     }
 
     fetchOrder()
-  }, [networkId, orderId])
+  }, [networkId, orderId, forcedUpdate])
 
-  return { order, isLoading, error }
+  return { order, isLoading, error, forceUpdate }
 }
 
-type Result = {
+type UseOrderAndErc20sResult = {
   order: Order | null
   buyToken: TokenErc20 | null
   sellToken: TokenErc20 | null
@@ -52,13 +62,15 @@ type Result = {
 
 /**
  * Aggregates the fetching of the order and related erc20s
+ * Optionally sets an interval of how often to update Open orders
  *
  * @param orderId The order id
+ * @param updateInterval [Optional] How often should try to update the order
  */
-export function useOrderAndErc20s(orderId: string): Result {
+export function useOrderAndErc20s(orderId: string, updateInterval = 0): UseOrderAndErc20sResult {
   const networkId = useNetworkId() || undefined
 
-  const { order, isLoading: isOrderLoading, error: orderError } = useOrder(orderId)
+  const { order, isLoading: isOrderLoading, error: orderError, forceUpdate } = useOrder(orderId)
   const addresses = order ? [order.buyTokenAddress, order.sellTokenAddress] : []
 
   // TODO: consume errors obj
@@ -66,6 +78,22 @@ export function useOrderAndErc20s(orderId: string): Result {
 
   const buyToken = value && value[order?.buyTokenAddress || '']
   const sellToken = value && value[order?.sellTokenAddress || '']
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | null = null
+
+    // Only start the interval when:
+    // 1. Hook is configured to do so (`updateInterval` > 0)
+    // 2. Order exists
+    // 3. Order is not expired
+    if (updateInterval && order && order.expirationDate.getTime() > Date.now()) {
+      intervalId = setInterval(forceUpdate, updateInterval)
+    }
+
+    return (): void => {
+      intervalId && clearInterval(intervalId)
+    }
+  }, [forceUpdate, order, updateInterval])
 
   return { order, buyToken, sellToken, isLoading: isOrderLoading || areErc20Loading, error: orderError }
 }
