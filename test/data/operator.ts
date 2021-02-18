43a32145fbfb9a17e90f6b9658608f79547603de
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-18 14:17:33 -0800

    New test data RICH_ORDER, and renamed ORDER to RAW_ORDER

--- test/data/operator.ts
+++ test/data/operator.ts
@@ -1,8 +1,8 @@
-import { RawOrder } from 'api/operator'
+import { Order, RawOrder, transformOrder } from 'api/operator'
 
-import { FEE_TOKEN } from './basic'
+import { USDT, WETH } from './erc20s'
 
-export const ORDER: RawOrder = {
+export const RAW_ORDER: RawOrder = {
   creationDate: '2021-01-20T23:15:07.892538607Z',
   owner: '0x5b0abe214ab7875562adee331deff0fe1912fe42',
   uid: 'asdasdasd',
@@ -13,8 +13,8 @@ export const ORDER: RawOrder = {
   feeAmount: '0',
   executedFeeAmount: '0',
   invalidated: false,
-  sellToken: '0xa7d1c04faf998f9161fc9f800a99a809b84cfc9d',
-  buyToken: FEE_TOKEN,
+  sellToken: WETH.address,
+  buyToken: USDT.address,
   validTo: 0,
   appData: 0,
   kind: 'sell',
@@ -22,3 +22,5 @@ export const ORDER: RawOrder = {
   signature:
     '0x04dca25f59e9ac744c4093530a38f1719c4e0b1ce8e4b68c8018b6b05fd4a6944e1dcf2a009df2d5932f7c034b4a24da0999f9309dd5108d51d54236b605ed991c',
 }
+
+export const RICH_ORDER: Order = { ...transformOrder(RAW_ORDER), buyToken: WETH, sellToken: USDT }
