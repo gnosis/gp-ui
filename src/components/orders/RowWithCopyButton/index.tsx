c8857df8157f34665244677d2e734a2e586f9717
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-26 07:19:05 -0800

    Allow RowWithCopyButton to be styled

--- src/components/orders/RowWithCopyButton/index.tsx
+++ src/components/orders/RowWithCopyButton/index.tsx
@@ -15,16 +15,17 @@ const Wrapper = styled.span`
 type Props = {
   textToCopy: string
   contentsToDisplay: string | JSX.Element
+  className?: string
 }
 
 export function RowWithCopyButton(props: Props): JSX.Element {
-  const { textToCopy, contentsToDisplay } = props
+  const { textToCopy, contentsToDisplay, className } = props
 
   // Wrap contents in a <span> if it's a raw string for proper CSS spacing
   const contentsComponent = typeof contentsToDisplay === 'string' ? <span>{contentsToDisplay}</span> : contentsToDisplay
 
   return (
-    <Wrapper>
+    <Wrapper className={className}>
       {contentsComponent} <CopyButton text={textToCopy} />
     </Wrapper>
   )
