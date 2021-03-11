25c638ff1e20ec3c20a9ead6c3222441ea420dcb
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-03-10 16:44:00 -0800

    Refactored magic number

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -2,7 +2,14 @@ import BigNumber from 'bignumber.js'
 
 import { TokenErc20, formatSmart } from '@gnosis.pm/dex-js'
 
-import { ONE_HUNDRED_BIG_NUMBER, BATCH_TIME_IN_MS, DEFAULT_DECIMALS, ONE_BIG_NUMBER, TEN_BIG_NUMBER } from 'const'
+import {
+  ONE_HUNDRED_BIG_NUMBER,
+  BATCH_TIME_IN_MS,
+  DEFAULT_DECIMALS,
+  ONE_BIG_NUMBER,
+  TEN_BIG_NUMBER,
+  MINIMUM_ATOM_VALUE,
+} from 'const'
 import { batchIdToDate } from './time'
 
 export {
@@ -214,7 +221,7 @@ export function capitalize(sentence: string): string {
 export function getMinimumRepresentableValue(decimals?: number): string {
   // Small limit === 1 token atom in relation to token units.
   // E.g.: Token decimals: 5; 1 unit => 100000; 1 atom => 0.00001 === small limit
-  return decimals ? ONE_BIG_NUMBER.div(TEN_BIG_NUMBER.exponentiatedBy(decimals)).toString(10) : '1'
+  return decimals ? ONE_BIG_NUMBER.div(TEN_BIG_NUMBER.exponentiatedBy(decimals)).toString(10) : MINIMUM_ATOM_VALUE
 }
 
 /**
