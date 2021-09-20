649e7e606fbe51cb897974bb07c0ce9d19b8e2b7
Henry Palacios <henrydpalacios@gmail.com>
2021-09-20 16:10:47 -0300

    Remove unnecessary arrow function in the test

--- test/utils/operator/orderFormatAmount.test.ts
+++ test/utils/operator/orderFormatAmount.test.ts
@@ -4,20 +4,16 @@ import { isTokenErc20, formattedAmount } from 'utils'
 
 import { TokenErc20 } from '@gnosis.pm/dex-js'
 
-const factoryToken = (): TokenErc20 => {
-  return {
-    symbol: 'WETH',
-    name: 'Wrapped Ether',
-    address: '0xc778417e063141139fce010982780140aa0cd5ab',
-    decimals: 2,
-  }
+const WEthToken: TokenErc20 = {
+  symbol: 'WETH',
+  name: 'Wrapped Ether',
+  address: '0xc778417e063141139fce010982780140aa0cd5ab',
+  decimals: 2,
 }
 
 describe('Is token an ERC20', () => {
   test('should return true when it complies with TokenERC20 interface', () => {
-    const token = factoryToken()
-
-    expect(isTokenErc20(token)).toBe(true)
+    expect(isTokenErc20(WEthToken)).toBe(true)
   })
 
   test('should return false when object is undefined', () => {
@@ -29,7 +25,7 @@ describe('Is token an ERC20', () => {
 
 describe('format amount', () => {
   test('should return a string when input is a erc20 and amount', () => {
-    const amount = formattedAmount(factoryToken(), new BigNumber('1'))
+    const amount = formattedAmount(WEthToken, new BigNumber('1'))
 
     expect(amount).toEqual('0.01')
   })
