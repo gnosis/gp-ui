db4454a4646377cfa73b7b8a51b13823514e72df
Agustín Longoni <agustin.longoni@altoros.com>
2021-09-22 10:18:01 -0300

    add styles and fix any type

--- src/components/orders/OrderNotFound/index.tsx
+++ src/components/orders/OrderNotFound/index.tsx
@@ -8,23 +8,25 @@ const Wrapper = styled.div`
   font-size: 14px;
 `
 
-const Title = styled.p`
+const Title = styled.h1`
   font-size: 16px;
-  font-weight: 600;
+  padding: 2.4rem 0 2.35rem;
+  font-weight: ${({ theme }): string => theme.fontBold};
 `
 
 const Content = styled.div`
   font-size: 14px;
-  border: 1px solid #777;
-  padding: 1rem;
+  border: 0.1rem solid ${({ theme }): string => theme.borderPrimary};
+  padding: 20px;
+  border-radius: 0.4rem;
 `
 
 const Errors = styled.div`
-  border-radius: 4px;
   padding: 1rem 1rem 2rem;
-  background: #333;
+  background: #36080847;
   margin-top: 3rem;
   width: 600px;
+  border-radius: 0.4rem;
 `
 
 const ErrorItem = styled.div`
@@ -40,34 +42,38 @@ const SearchContent = styled.div`
   display: flex;
 `
 
-const Support = styled.div`
+const Support = styled.a`
   height: 5rem;
-  border: 1px solid #777;
+  border: 1px solid ${({ theme }): string => theme.borderPrimary};
   border-radius: 4px;
   width: 150px;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  :hover {
+    background-color: ${({ theme }): string => theme.bg1};
+  }
 `
 
 export const OrderNotFound: React.FC = () => {
-  const params: any = useParams()
+  const { orderId } = useParams<{ orderId: string }>()
 
   return (
     <Wrapper>
       <Title>Order not found</Title>
       <Content>
-        <p>Sorry, no matches found for: {params.orderId}</p>
+        <p>Sorry, no matches found for: {orderId}</p>
         <Errors>
           <p>Errors:</p>
-          <ErrorItem>Failed to fetch order: {params.orderId}</ErrorItem>
+          <ErrorItem>Failed to fetch order: {orderId}</ErrorItem>
         </Errors>
         <SearchSection>
           <Title>Search again by Order ID</Title>
           <SearchContent>
             <Search />
-            <p style={{ margin: 'auto 3rem' }}>Or</p>
-            <Support>
+            <p style={{ margin: 'auto 3rem' }}>or</p>
+            <Support href="#">
               Get Support
               <img style={{ marginLeft: '1rem' }} src={SupportIcon} />
             </Support>
