3a9c969daa7f612aa576ba690b8d482a25450e28
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-11-18 15:28:45 -0800

    Added `isBarn` variable and associated changes

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ const EXPLORER_APP = {
     EXPLORER_APP_DOMAIN_REGEX_DEV: '^protocol-explorer\\.dev|^localhost:\\d{2,5}|^pr\\d+--gpui\\.review',
     EXPLORER_APP_DOMAIN_REGEX_STAGING: '^protocol-explorer\\.staging',
     EXPLORER_APP_DOMAIN_REGEX_PROD: '^gnosis-protocol\\.io',
+    EXPLORER_APP_DOMAIN_REGEX_BARN: '^barn\\.gnosis-protocol\\.io',
 
     OPERATOR_URL_STAGING_MAINNET: 'https://protocol-mainnet.dev.gnosisdev.com/api',
     OPERATOR_URL_STAGING_RINKEBY: 'https://protocol-rinkeby.dev.gnosisdev.com/api',
