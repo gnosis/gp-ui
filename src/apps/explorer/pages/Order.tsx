b37a09f248c194b7989acbf467cf484502ec6f06
Henry Palacios <henrydpalacios@gmail.com>
2021-11-25 11:53:57 -0300

    Redirect to search when orderId is Invalid

--- src/apps/explorer/pages/Order.tsx
+++ src/apps/explorer/pages/Order.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 import { useOrderIdParam } from 'hooks/useSanitizeOrderIdAndUpdateUrl'
-import NotFound from 'apps/explorer/pages/NotFound'
 import { isAnOrderId } from 'utils'
 
-import { OrderWidget } from '../components/OrderWidget'
+import RedirectToSearch from 'components/RedirectToSearch'
+import { OrderWidget } from 'apps/explorer/components/OrderWidget'
 import { WrapperPage } from './styled'
 
 const Order: React.FC = () => {
   const orderId = useOrderIdParam()
 
   if (!isAnOrderId(orderId)) {
-    return <NotFound />
+    return <RedirectToSearch from="orders" />
   }
 
   return (
