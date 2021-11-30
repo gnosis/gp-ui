88acbe12669267420702c87c01bc005d6db0dc01
infiniteMajor <maria.yablonskaya@altoros.com>
2021-11-30 19:14:49 +0300

    transaction view + adjusts

--- src/apps/explorer/pages/Home/index.tsx
+++ src/apps/explorer/pages/Home/index.tsx
@@ -1,35 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Search } from 'apps/explorer/components/common/Search'
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-flow: column wrap;
-  height: calc(100vh - 15rem);
-  padding: 1.6rem;
-  margin: 0 auto;
-  width: 100%;
-
-  > h1 {
-    display: flex;
-    padding: 2.4rem 0 0.75rem;
-    align-items: center;
-    justify-content: center;
-    font-weight: ${({ theme }): string => theme.fontBold};
-    width: 100%;
-    margin: 0 0 2.4rem;
-    font-size: 2.4rem;
-    line-height: 1;
-  }
-`
+import { HomeWrapper } from 'apps/explorer/pages/styled'
 
 export const Home: React.FC = () => {
   return (
-    <Wrapper>
+    <HomeWrapper>
       <h1>Search Order ID / ETH Address / ENS Address</h1>
       <Search className="home" />
-    </Wrapper>
+    </HomeWrapper>
   )
 }
 
--- src/apps/explorer/pages/styled.tsx
+++ src/apps/explorer/pages/styled.tsx
@@ -6,6 +6,7 @@ export const Wrapper = styled.div`
   padding: 1.6rem;
   margin: 0 auto;
   width: 100%;
+  flex-grow: 1;
 
   ${media.mediumDown} {
     max-width: 94rem;
@@ -31,10 +32,28 @@ export const WrapperPage = styled(Wrapper)`
   }
 `
 
+export const HomeWrapper = styled(WrapperPage)`
+  flex-flow: column wrap;
+  justify-content: center;
+  display: flex;
+
+  > h1 {
+    justify-content: center;
+    padding: 2.4rem 0 0.75rem;
+    width: 100%;
+    margin: 0 0 2.4rem;
+    font-size: 2.4rem;
+    line-height: 1;
+  }
+`
+
 export const TitleAddress = styled(RowWithCopyButton)`
   font-size: ${({ theme }): string => theme.fontSizeDefault};
   font-weight: ${({ theme }): string => theme.fontNormal};
   margin: 0 0 0 1.5rem;
   display: flex;
   align-items: center;
+  ${media.tinyDown} {
+    font-size: 1.2rem;
+  }
 `
