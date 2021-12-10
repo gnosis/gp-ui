00ff919b2c5baf85e47c1fc46f21209413e99848
Mati Dastugue <mdastugu@amazon.com>
2021-12-10 16:58:46 -0300

    Add tests to increase coverage level

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -15,6 +15,5 @@ export function buildSearchString(params: Record<string, string | undefined>): s
 export function replaceURL(url: string, strReplace: string): string {
   const re = /(([\w]+\/){0})([^/]+)(\/.+)*/gm
   const subst = '$1' + strReplace + '$4'
-  console.log('Romiii', url.replace(re, subst))
   return url.replace(re, subst)
 }
--- test/utils/url.spec.ts
+++ test/utils/url.spec.ts
@@ -1,4 +1,4 @@
-import { replaceURL } from 'utils/url'
+import { replaceURL, buildSearchString } from 'utils/url'
 
 describe('replace URL path according to network', () => {
   test('path is replaced to rinkeby', () => {
@@ -14,5 +14,16 @@ describe('replace URL path according to network', () => {
   test('url is replaced to mainnet', () => {
     const currentPath = '/xdai/address/0xb6bad41ae76a11d10f7b0e664c5007b908bc77c9'
     expect(replaceURL(currentPath, 'mainnet')).toBe('/mainnet/address/0xb6bad41ae76a11d10f7b0e664c5007b908bc77c9')
+  })
+})
+
+describe('BuildSearchString', () => {
+  test('params decoded correctly', () => {
+    const owner = '0xbD326ba3D9BaD3A08cf521C41bC69A620a750B3f'
+    const orderId =
+      '0x59920c85de0162e9e55df8d396e75f3b6b7c2dfdb535f03e5c807731c31585eaff714b8b0e2700303ec912bd40496c3997ceea2b616d6710'
+    const queryString = `/trades` + buildSearchString({ owner, orderUid: orderId })
+
+    expect(queryString).toBe(`/trades?owner=${owner}&orderUid=${orderId}`)
   })
 })
